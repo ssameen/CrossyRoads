e60794e22905b568b9593ed29453b608a15c709c
Mustafa Sameen <s_sameen@coloradocollege.edu>
2022-11-10 10:55:56 -0700

    created Obstacle class

---
+++ Obstacle.java
@@ -0,0 +1,32 @@
+public class Obstacle{
+    private boolean canStep, willDie;
+    private String imagePath;
+    private int x, y, size;
+
+    public Obstacle(int x, int y, int size, boolean canStep, boolean willDie, String imagePath){
+        this.canStep = canStep;
+        this.willDie = willDie;
+        this.imagePath = imagePath;
+        this.x = x;
+        this.y = y;
+        this.size = size;
+    }
+
+    private Square getSquare(){
+        return new Square(x, y, size);
+    }
+
+    private boolean checkCollision(Player player){
+        Square playerSquare = player.getSquare();
+        Square obstacleSquare = this.getSquare();
+        int x1 = playerSquare.getBottomLeft()[0], y1 = playerSquare.getBottomLeft()[1];
+        int x2 = playerSquare.getTopRight()[0], y2 = playerSquare.getTopRight()[1];
+        int x3 = obstacleSquare.getBottomLeft()[0], y3 = obstacleSquare.getBottomLeft()[1];
+        int x4 = obstacleSquare.getTopRight()[0], y4 = obstacleSquare.getTopRight()[1];
+        return (x1 < x4) && (x3 < x2) && (y1 < y4) && (y3 < y2);
+    }
+
+    public boolean checkDeath(Player player){
+        return willDie && checkCollision(player);
+    }
+}
--- Player.java
+++ Player.java
@@ -1,3 +1,11 @@
-public class Player extends Square { 
-  
-} 
+public class Player extends Square {
+    int x, y, size;
+
+    public Player(int x, int y, int size) {
+        super(x, y, size);
+    }
+
+    public Square getSquare(){
+        return new Square(x, y, size);
+    }
+}
