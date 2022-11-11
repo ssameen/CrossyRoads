ae269f4fb81af91e0d84b3cb13530d97bf3360e8
Kiernan Nesslar <nesmcq@gmail.com>
2022-11-10 21:27:50 -0700

    few updates -- ready to hook up to obstacle classes once that's all figured out.

--- Game.java
+++ Game.java
@@ -1,13 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 
-public class Game extends JComponent {
-
+public class Game extends JComponent implements KeyListener {
+    Random rand = new Random();
     static Boolean death = false;
-    Player chicken = new Player();
+    Player chicken = new Player(365,550, 75,);
     static int score = 0;
+    static int position = 0;
     NodeQueue enviro = new NodeQueue();
 
     @Override
@@ -33,15 +37,23 @@ public class Game extends JComponent {
     public void paintWorlds(Graphics2D g2){
         for(int i =0; i < enviro.getSize(); i++){
             World now = enviro.check(i);
-            g2.setColor(now.c);
+            g2.setColor(now.getBackground());
             g2.fillRect(now.x+100, enviro.getSize()*now.height-(i*50)+100, now.width, now.height);
+
+        }
+    }
+
+    void paintObstacles(Graphics2D g2){
+        for(int i =0; i < enviro.getSize(); i++){
+            World now = enviro.check(i);
+            Obstacle[] obs = now.getObstacles();
 
         }
     }
 
     public Game(){
         for(int i =0; i<6;i++){
-            World next = new World();
+            World next = newWorld();
             enviro.enqueue(next);
         }
 
@@ -57,16 +69,41 @@ public class Game extends JComponent {
     }
 
     void tick(){
-        score++;
+
+
         enviro.dequeue();
-        World next = new World();
+        World next = newWorld();
         enviro.enqueue(next);
+        position--;
+        moveObjects();
         this.repaint();
+    }
+
+    void moveObjects(){
+        for(int i =0; i < enviro.getSize(); i++){
+            World now = enviro.check(i);
+
+            now.moveObstacle();
+        }
+    }
+
+    World newWorld(){
+        World next;
+        if(rand.nextInt(4)==0){
+            next = new Road();
+        }else if(rand.nextInt(4) == 1){
+            next = new Forest();
+        }else if(rand.nextInt(4)==2){
+            next = new Railroad();
+        }else{
+            next = new River();
+        }
+        return next;
     }
 
     public static void main(String[] args) throws InterruptedException {
         JFrame frame = new JFrame();
-        frame.setSize(630, 600);
+        frame.setSize(730, 600);
         frame.setLocation(0, 0);
         Game g = new Game();
         frame.add(g);
@@ -75,5 +112,49 @@ public class Game extends JComponent {
         frame.setVisible(true);
 
         g.run();
+    }
+    private void playerUp() {
+        if(!obsAbove()) {
+            chicken.moveUp();
+            position++;
+            score++;
+            this.repaint();
+        }
+    }
+
+    private boolean obsAbove(){
+        World above = enviro.check(position +1);
+        Obstacle[] obs = above.getObstacles();
+        for(Obstacle o: obs){
+            int[] values = new int[2];
+            values[0] = o.getBottomLeft[0];
+            values[1] = o.getTopRight[0];
+            if(chicken.getTopRight()[0] <= values[1]&&chicken.getTopRight()[0]>=values[0]){
+                return true;
+            }else if(chicken.getBottomLeft()[0] <= values[1]&&chicken.getBottomLeft()[0]>=values[0]){
+                return true;
+            }
+        }
+        return false;
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int KeyCode = e.getKeyCode();
+        if(KeyCode == KeyEvent.VK_UP){
+            playerUp();
+
+        }
+    }
+
+
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
     }
 }
